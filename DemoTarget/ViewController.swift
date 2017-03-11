af3c0f02f757a9ed368437c1f2777125af2efcbb
Omar Juarez <omarojocs@gmail.com>
2017-03-10 17:47:37 -0800

    working

---
+++ DemoTarget/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  DemoTarget
+//
+//  Created by Omar Juarez Ortiz on 2017-03-10.
+//  Copyright © 2017 Omar Juarez Ortiz. All rights reserved.
+//
+
+import UIKit
+import MyC4FW
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        
+        self.present(SquareVC.init(), animated: true, completion: nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
